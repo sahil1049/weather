e423ee0193926b066528a1fd246e85061eb71fed
Sahil Khurana <47140401+sahil1049@users.noreply.github.com>
2020-06-13 22:23:39 +0530

    setup for heroku

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const weather=require('./utils/weather')
 // console.log(__dirname)
 // console.log(path.join(__dirname,'../public'))
 const app=express()
-
+const port =process.env.PORT || 3000
 //Defining paths for express config
 const viewsPath=path.join(__dirname,'../templates/views')
 const publicdir=path.join(__dirname,'../public')
@@ -104,6 +104,6 @@ app.get('*',(req,res)=>{
     })
 
 
-app.listen(3000,()=>{
-    console.log('Server is up on port 3000')
+app.listen(port,()=>{
+    console.log('Server is up on port'+port)
 })
